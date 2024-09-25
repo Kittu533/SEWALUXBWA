512bb9c64e933c1f3132298336c17cb9870a9c23
Ryobi <robiatmaja7@gmail.com>
2024-09-25 22:08:38 +0700

    feat : dashboard Control

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'trx_id',
+        'proof',
+        'phone_number',
+        'address',
+        'total_amount',
+        'product_id',
+        'store_id',
+        'duration',
+        'is_paid',
+        'delivery_type',
+        'started_at',
+        'ended_at',
+    ];
+
+    protected $casts = [
+        'total_amount' => MoneyCast::class,
+        'started_at' => 'date',
+        'ended_at' => 'date',
+    ];
+    // membuat random untuk trx_id
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'SLBWA';
+        do {
+            $randomString = $prefix . mt_rand(100000, 999999);
+        } while (Transaction::where('trx_id', $randomString)->exists());
+        return $randomString;
+    }
+
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
